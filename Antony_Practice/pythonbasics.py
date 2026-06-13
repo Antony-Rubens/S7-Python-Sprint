a38046b5df143b3907b7950e6b16146e95b07687
Antony-Rubens <145246163+Antony-Rubens@users.noreply.github.com>
2026-06-13 13:51:14 +0530

    day 3 arrays practice1

---
+++ Antony_Practice/pythonbasics.py
@@ -0,0 +1,17 @@
+name = "antony"
+age = 10
+score = 80
+print(name,age) 
+if score >= 90:
+    print(name)
+elif score == 80:
+    print(age)
+else:
+    print("sucess")
+for i in range(5):
+    print("help")
+count = 0
+while count < 3:
+    print("hi")
+    print(count)
+    count += 1
