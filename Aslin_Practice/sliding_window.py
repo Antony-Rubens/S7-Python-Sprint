889be651e4b4cae39be244f4d2264adb7d435648
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-06-18 23:47:00 +0530

    sliding window

--- Aslin_Practice/sliding_window.py
+++ Aslin_Practice/sliding_window.py
@@ -4,7 +4,7 @@ def find_max_average(nums, k):
 
     for i in range(k,len(nums)):
         current_sum+=nums[i]-nums[i-k]
-        max_sum += max(max_sum, current_sum)
+        max_sum = max(max_sum, current_sum)
 
     return max_sum / k
 
