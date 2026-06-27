315d39a8fe216dec24927a5d7893313279265cbe
Aslin Yeasudas <aslinyesudas@gmail.com>
2026-06-27 23:07:47 +0530

    encode decode

---
+++ Aslin_Practice/encode_decode.py
@@ -0,0 +1,24 @@
+class Solution:
+    def encode(self, strs:[list[str]])-> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+            return res
+    
+    def decode(self, s:str) -> list[str]:
+        res=[]
+        i=0
+
+        while i < len(s):
+            j=1
+
+            while s[j] != "#":
+                j +=1
+
+            length=int(s[i:j])
+
+            res.append(s[j+1:j+1+length])    
+
+            i=j+1+length
+
+        return res
